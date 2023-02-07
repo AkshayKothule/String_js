986c5d1f1cd7cf9e20e234ce5555cd2e78f2989d
Akshay Kothule <>
2023-02-07 11:56:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let name="Akshay";
+console.log(name);
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[1]);
+console.log(name.length);
+let name1='kothule';
+console.log(name1);
+
+// itreation of string 
+for(let i=0 ; i<name1.length ;i++)
+  {
+    console.log(name1[i]);
+  }
+
+// templete litrals
+
+let boy1="Akshay";
+let boy2="Somesh";
+
+//somesh is a friend of akshay
+let senstence=`${boy2} is friend of ${boy1}`;
+console.log(senstence);
+
+//escape character 
